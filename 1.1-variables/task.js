a40ed857f19e8c8b1ccf559d0eea79a398100d6d
Oksana Zhekalova <oks.gek@gmail.com>
2020-12-18 15:22:34 +0300

    homework 1.1 is done

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,24 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	let algebra = a, geography = g, physics = p;
+	let avg = (algebra + geography + physics) / 3;
     
-	return // ваш ответ;
+	return avg;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+	let myName = userName;
+	message = `Привет, мир! Меня зовут ${userName}`;
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2, y = 22, z = 0;
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result;
 }
