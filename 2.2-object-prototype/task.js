f5c5f8cd0791454ab1e8710db46aae89e0ff8180
Oksana Zhekalova <oks.gek@gmail.com>
2021-02-19 14:10:45 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,52 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+String.prototype.isPalindrome = function(str) {
+    str = this.toLowerCase();
+    console.log(str);
+
+    let newStr = str.split('');
+    newStr.forEach((word) => {
+        if (word === ' ') {
+          newStr.splice(newStr.indexOf(' '),1);
+        }
+      });
+    newStr = newStr.join();
+
+    let strReverse = newStr.split('').reverse().join('');
+      
+    if (newStr === strReverse) { 
+        return true;
+    } else { 
+        return false; 
+    };
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    };
+
+    let sumValues = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sumValues += marks[i];
+    };
+    
+    let average = sumValues / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let todayDate = new Date();
+    let now = todayDate.getTime();
+    console.log(`today: ${now}`);
+
+    let  userBirthday = new Date(birthday);
+    birthday = userBirthday.getTime();
+    console.log(`userBirthday: ${birthday}`);
+
+    let diff = now - birthday;
+    let age = Math.trunc(diff / 31556952000);
+
+    console.log(`result: ${(age > 18)}`);
+    return (age > 18);
 }
